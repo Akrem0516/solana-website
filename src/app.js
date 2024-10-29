9079cc14e7c42ac8d871b86a8a754f7197b99ff9
akrem0516 <ouassimakrem@gmail.com>
2024-10-29 16:19:33 +0100

    port changes

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const app = express();
 
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT , () => {
     console.log(`app is running on PORT ${PORT}`);
