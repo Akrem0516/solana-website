d732dd709a94bea42a68ff2c4b48b48099563d37
akrem0516 <ouassimakrem@gmail.com>
2024-11-02 14:53:57 +0100

    change content styling

--- public/JS/loading.js
+++ public/JS/loading.js
@@ -3,6 +3,7 @@ window.addEventListener("load", function() {
     // Delay for 2 seconds (2000 milliseconds)
     setTimeout(function() {
         document.getElementById("loading-screen").style.display = "none";
-        document.getElementById("content").style.display = "block";
+        document.getElementById("content").style = "";
+
     }, 2000);
 });
