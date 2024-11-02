c98424172d14343fb97ccb45d45ac9781c255894
akrem0516 <ouassimakrem@gmail.com>
2024-11-02 13:59:12 +0100

    loading

---
+++ public/JS/loading.js
@@ -0,0 +1,8 @@
+
+window.addEventListener("load", function() {
+    // Delay for 2 seconds (2000 milliseconds)
+    setTimeout(function() {
+        document.getElementById("loading-screen").style.display = "none";
+        document.getElementById("content").style.display = "block";
+    }, 2000);
+});
